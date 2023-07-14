d54d11a59b110c28d164351f2debf595838441c4
Pablo Roldão <pablo.roldao.santos@gmail.com>
2023-07-13 23:42:45 -0300

    first commit

---
+++ resources/views/series/create.blade.php
@@ -0,0 +1,10 @@
+<x-layout title="Nova série">
+    <form action="" method="post">
+        <div class="mb-2">
+            <label for="nome" class="form-label">Nome:</label>
+            <input type="text" id="nome" name="nome" class="form-control">
+        </div>
+
+        <button type="submit" class="btn btn-dark">Adicionar</button>
+    </form>
+</x-layout>
